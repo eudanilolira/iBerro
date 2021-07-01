75838f041860b5305755f4cdf8a23c6a1903cfc5
Danilo Lira <danilo.lira01@gmail.com>
2021-07-01 09:00:47 -0300

    Project setup

---
+++ iBerro/Views/Menu.swift
@@ -0,0 +1,20 @@
+//
+//  Menu.swift
+//  iBerro
+//
+//  Created by Danilo Araújo on 01/07/21.
+//
+
+import SwiftUI
+
+struct Menu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Menu_Previews: PreviewProvider {
+    static var previews: some View {
+        Menu()
+    }
+}
